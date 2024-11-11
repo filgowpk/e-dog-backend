cb2229ce6328b09a403cc26d8434c0470ed90f85
dkwapisz <dkwapisz99@gmail.com>
2024-11-11 12:19:40 +0100

    [KYN-13] Initial commit

---
+++ src/main/java/org/pk/edog/EDogApplication.java
@@ -0,0 +1,13 @@
+package org.pk.edog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pk/edog/EDogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pk.edog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
